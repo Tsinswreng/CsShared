6e6c1b5ddb2da0fdafce921bc559f9b4029035f3
Tsinswreng <2290019838@qq.com>
2025-03-04 11:05:16 +0800

    Update PtrUtil.cs

--- Interop/PtrUtil.cs
+++ Interop/PtrUtil.cs
@@ -1,6 +1,12 @@
+using static System.Runtime.InteropServices.NativeMemory;
+using static System.Runtime.InteropServices.Marshal;
 namespace Shr.Interop;
 unsafe public static class PtrUtil{
 	public static T* New<T>()where T:unmanaged{
 		return (T*)AllocZeroed((nuint)SizeOf<T>());
+	}
+
+	public int SizeOf_int<T>()where T:unmanaged{
+		return SizeOf<T>();
 	}
 }
