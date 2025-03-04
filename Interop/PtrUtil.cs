bf393d183f27521b71a424fb0647d4ffeb37fcf8
Tsinswreng <2290019838@qq.com>
2025-03-04 22:56:02 +0800

    改FreeEtNull

--- Interop/PtrUtil.cs
+++ Interop/PtrUtil.cs
@@ -6,7 +6,39 @@ unsafe public static class PtrUtil{
 		return (T*)AllocZeroed((nuint)SizeOf<T>());
 	}
 
-	public int SizeOf_int<T>()where T:unmanaged{
+	public static zero FreeEtNull<T>(ref T* ptrRef)where T:unmanaged{
+		if(ptrRef == null){
+			return 0;
+		}
+		Free(ptrRef);
+		ptrRef = null;
+		return 0;
+	}
+
+
+	// public static zero FreeIfNotNull<T>(T* ptr)where T:unmanaged{
+	// 	if(ptr != null){
+	// 		Free(ptr);
+	// 	}
+	// 	return 0;
+	// }
+
+	///
+	/// int* ptr = xxx
+	/// FreeEtNull(&ptr)
+	/// sometimes needs to use fixed{}
+	///
+	// public static zero FreeEtNull<T>(T** ptr2d)where T:unmanaged{
+	// 	var ptr = *ptr2d;
+	// 	if(ptr == null){
+	// 		return 0;
+	// 	}
+	// 	Free(ptr);
+	// 	*ptr2d = null;
+	// 	return 0;
+	// }
+
+	public static int SizeOf_int<T>()where T:unmanaged{
 		return SizeOf<T>();
 	}
 }
